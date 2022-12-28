7c69f40952ab23bd859c383a6a501b0e6f5128ad
Mariusz Jacek <marios.yaceg@gmail.com>
2022-12-28 18:15:48 +0100

    component's HTMl

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,17 @@ import '../App.css';
 export default function About(){
     return(
         <div className="aboutMe">
-            
+            <div className="aboutMeCoding">
+                <div className="aboutMeCodigVectors"></div>
+                <div className="aboutMeCodingText"></div>
+            </div>
+            <div className="aboutMeCodingTimestamps">
+            </div>
+            <div className="aboutMeSchool">
+                <div className="aboutMeSchoolContestsVectors"></div>
+                <div className="aboutMeSchoolText"></div>
+            </div>
+            <div className="aboutMeFantasy"></div>
         </div>
     )
 }
