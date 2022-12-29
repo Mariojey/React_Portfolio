6070159e71923ef9b8ad4bb04383b28aca7d12e0
Mariusz Jacek <marios.yaceg@gmail.com>
2022-12-29 17:24:32 +0100

    Add Vectors

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -18,14 +18,20 @@ export default function About(){
     return(
         <div className="aboutMe">
             <div className="aboutMeCoding">
-                <div className="aboutMeCodigVectors"></div>
+                <div className="aboutMeCodigVectors">
+                    <img src="../images/node.svg" alt="node.js" className="aboutMeVectorImg"/>
+                    <img src="../images/js.svg" alt="js" className="aboutMeVectorImg"/>
+                    <img src="../images/mongodb.svg" alt="mongo" className="aboutMeVectorImg"/>
+                </div>
                 <div className="aboutMeCodingText">{textAboutMe[0]}</div>
             </div>
             <div className="aboutMeCodingTimestamps">
             </div>
             <div className="aboutMeSchool">
                 <div className="aboutMeSchoolText">{textAboutMe[1]}</div>
-                <div className="aboutMeSchoolContestsVectors"></div>
+                <div className="aboutMeSchoolContestsVectors">
+                <img src="../images/python.svg" alt="python" className="aboutMeVectorImg"/>
+                </div>
             </div>
             <div className="aboutMeFantasy"></div>
             <h1 className='cardsHeader'>My Jobs</h1>
