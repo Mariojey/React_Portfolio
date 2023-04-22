fc2fdaa59389dd39164c1c3a5e7fa52765a4eaff
Mariusz Jacek <marios.yaceg@gmail.com>
2023-04-22 12:56:28 +0200

    Update About Me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -34,7 +34,7 @@ export default function About(){
 
     return(
         <div className="aboutMe" id="aboutMe">
-            <div className="aboutMeCoding">
+            {/* <div className="aboutMeCoding">
                 <div className="aboutMeCodigVectors">
                     <img src="./images/node.svg" alt="node.js" className="aboutMeVectorImg"/>
                     <img src="./images/js.svg" alt="js" className="aboutMeVectorImg"/>
@@ -51,16 +51,34 @@ export default function About(){
                     <img src="./images/typescript.svg" alt="python" className="aboutMeVectorImgSchool"/>
                     <img src="./images/react.svg" alt="python" className="aboutMeVectorImgSchool"/>
                 </div>
+            </div> */}
+            <div className="aboutMeContainer">
+                <>
+                <div className="aboutMePattern patternNode">
+                    <img src="./images/node.svg" alt="node.js" className="aboutMeVectorImg"/>
+                </div>
+                <div className="aboutMePattern patternJavaScript">
+                    <img src="./images/js.svg" alt="js" className="aboutMeVectorImg"/>
+                </div>
+                <div className="aboutMePattern patternMongodb">
+                    <img src="./images/mongodb.svg" alt="mongo" className="aboutMeVectorImg"/>
+                </div>
+                <div className="aboutMePattern patternReact">
+                    <img src="./images/react.svg" alt="python" className="aboutMeVectorImg"/>
+                </div>
+                </>
+                <div className="aboutMeCodingText">{textAboutMe[0]}</div>
+
             </div>
             <div className="aboutMeFantasy"></div>
             <h1 className='cardsHeader'>My Experience</h1>
             <div className="experience">
-            <div className="jobs">{jobs}</div>
-            <div className="experienceText">
-                <div className="typing">
-                <span ref={el} />
+                <div className="jobs">{jobs}</div>
+                <div className="experienceText">
+                    <div className="typing">
+                    <span ref={el} />
+                    </div>
                 </div>
-            </div>
             </div>
             
         </div>
